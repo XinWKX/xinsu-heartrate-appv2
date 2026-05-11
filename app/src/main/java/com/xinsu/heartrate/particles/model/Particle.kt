4c481642aade12686898c7e7853d4ff56c25b138
XinWKX <1216265957@QQ.com>
2026-05-12 00:41:38 +0800

    Create Particle.kt

---
+++ app/src/main/java/com/xinsu/heartrate/particles/model/Particle.kt
@@ -0,0 +1,20 @@
+package com.xinsu.heartrate.particles.model
+
+data class Particle(
+
+    var x: Float,
+
+    var y: Float,
+
+    var radius: Float,
+
+    var alpha: Int,
+
+    var depth: Float,
+
+    var angle: Float,
+
+    var orbitRadius: Float,
+
+    var speed: Float
+)
