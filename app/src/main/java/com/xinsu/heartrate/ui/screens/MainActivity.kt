5788eaeb3cd2767f853cb88d3a68f908ce175f7d
XinWKX <1216265957@QQ.com>
2026-05-12 00:35:34 +0800

    Add MainActivity with UI initialization

---
+++ app/src/main/java/com/xinsu/heartrate/ui/screens/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.xinsu.heartrate.ui.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.xinsu.heartrate.ui.widgets.RootHudLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+
+        super.onCreate(savedInstanceState)
+
+        initUI()
+    }
+
+    /**
+     * 初始化 UI
+     */
+    private fun initUI() {
+
+        val root = RootHudLayout(this)
+
+        setContentView(root)
+    }
+}
