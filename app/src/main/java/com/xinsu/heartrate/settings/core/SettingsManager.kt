8ae9259b31b3dc01878ac8d877975b8f974de68d
XinWKX <1216265957@QQ.com>
2026-05-12 01:08:01 +0800

    Create SettingsManager.kt

---
+++ app/src/main/java/com/xinsu/heartrate/settings/core/SettingsManager.kt
@@ -0,0 +1,44 @@
+package com.xinsu.heartrate.settings.core
+
+object SettingsManager {
+
+    /**
+     * Pulse 音效
+     */
+    var pulseAudioEnabled = true
+
+    /**
+     * Haptic
+     */
+    var hapticEnabled = false
+
+    /**
+     * 粒子数量
+     */
+    var particleCount = 140
+
+    /**
+     * Glow 强度
+     */
+    var glowEnabled = true
+
+    /**
+     * ECG Glow
+     */
+    var ecgGlowEnabled = true
+
+    /**
+     * 低功耗模式
+     */
+    var lowPowerMode = false
+
+    /**
+     * FPS 限制
+     */
+    var targetFps = 60
+
+    /**
+     * 动态背景
+     */
+    var animatedBackground = true
+}
