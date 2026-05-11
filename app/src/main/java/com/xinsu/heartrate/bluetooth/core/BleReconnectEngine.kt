aa632842ac230f3461d07ce6dc7f552459953512
XinWKX <1216265957@QQ.com>
2026-05-12 02:16:21 +0800

    Add BleReconnectEngine for Bluetooth reconnection

---
+++ app/src/main/java/com/xinsu/heartrate/bluetooth/core/BleReconnectEngine.kt
@@ -0,0 +1,14 @@
+package com.xinsu.heartrate.bluetooth.core
+
+object BleReconnectEngine {
+
+    fun startReconnect() {
+
+        // TODO
+    }
+
+    fun stopReconnect() {
+
+        // TODO
+    }
+}
