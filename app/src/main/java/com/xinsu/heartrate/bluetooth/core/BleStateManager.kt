eab442b2215ee9786cfd0fe71f99ba67b1244ecc
XinWKX <1216265957@QQ.com>
2026-05-23 00:30:28 +0800

    Update BleStateManager.kt

--- app/src/main/java/com/xinsu/heartrate/bluetooth/core/BleStateManager.kt
+++ app/src/main/java/com/xinsu/heartrate/bluetooth/core/BleStateManager.kt
@@ -2,33 +2,19 @@ package com.xinsu.heartrate.bluetooth.core
 
 object BleStateManager {
 
-    private var currentState =
+    private var state =
         BleState.IDLE
 
-    private var listener:
-            ((BleState) -> Unit)?
-        = null
+    fun setState(
+        newState: BleState
+    ) {
+
+        state = newState
+    }
 
     fun getState():
             BleState {
 
-        return currentState
-    }
-
-    fun setState(
-        state: BleState
-    ) {
-
-        currentState = state
-
-        listener?.invoke(state)
-    }
-
-    fun setListener(
-        callback:
-        (BleState) -> Unit
-    ) {
-
-        listener = callback
+        return state
     }
 }
