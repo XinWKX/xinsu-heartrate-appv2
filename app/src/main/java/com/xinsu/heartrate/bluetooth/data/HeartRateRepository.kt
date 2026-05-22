c350b56ea57336397beeadd1464add62ab58809f
XinWKX <1216265957@QQ.com>
2026-05-23 00:08:26 +0800

    Add HeartRateRepository for managing heart rate data

---
+++ app/src/main/java/com/xinsu/heartrate/bluetooth/data/HeartRateRepository.kt
@@ -0,0 +1,11 @@
+package com.xinsu.heartrate.bluetooth.data
+
+object HeartRateRepository {
+
+    var currentBpm = 0
+
+    var connectedDeviceName =
+        "Disconnected"
+
+    var isConnected = false
+}
