d4a0b9b3f56f66aeeb43509abc8d468553efb7d6
XinWKX <1216265957@QQ.com>
2026-05-24 13:43:21 +0800

    Update BleState.kt

--- app/src/main/java/com/xinsu/heartrate/bluetooth/core/BleState.kt
+++ app/src/main/java/com/xinsu/heartrate/bluetooth/core/BleState.kt
@@ -2,15 +2,33 @@ package com.xinsu.heartrate.bluetooth.core
 
 enum class BleState {
 
+    /**
+     * 空闲
+     */
     IDLE,
 
+    /**
+     * 扫描中
+     */
     SCANNING,
 
+    /**
+     * 正在连接
+     */
     CONNECTING,
 
+    /**
+     * 已连接
+     */
     CONNECTED,
 
+    /**
+     * 已断开
+     */
     DISCONNECTED,
 
-    RECONNECTING
+    /**
+     * 连接失败
+     */
+    FAILED
 }
